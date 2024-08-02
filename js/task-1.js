26017ad92e83cea23ce1bee7423f32908fe9f785
Nelia Demenska <demenskaya@nelya.gmail.com>
2024-08-02 19:11:47 +0200

    js tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,16 @@
 function isEnoughCapacity(products, containerSize) {
-    
+    const values = Object.values(products);
+    let totalCapacity = 0;
+    for (const value of values) {
+        totalCapacity += value;
+    }
+    if (totalCapacity > containerSize) {
+        return false;
+    }
+    return true;  
 }
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
